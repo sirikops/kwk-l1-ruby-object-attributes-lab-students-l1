bf4b404d0cea223fa722e280c7cf8512505c78e8
sirikops <sirikops@hotmail.com>
2018-06-07 21:32:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+class Dog
+  
+  def initialize(name, breed)
+  @name=name
+  @breed=breed
+
+def name
+  @name=name
+ end
+def name=(name)
+  @name=name
+ end
+
+def breed
+  @breed=breed
+ end
+def breed=(breed)
+  @breed=breed
+ end
+end

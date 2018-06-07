1a7c4c05df01de3069b0959fd32f3c529a34adbb
sirikops <sirikops@hotmail.com>
2018-06-07 21:30:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # person.rb
+class Person
+  def initialize(name, job)
+    @name=name
+    @job=job
+    
+    def name
+      @name=name
+    end
+    def name=(name)
+      @name=name
+    end
+    
+    def job
+      @job=job
+    end
+    def job=(job)
+      @job=job
+    end
+    
+  end
